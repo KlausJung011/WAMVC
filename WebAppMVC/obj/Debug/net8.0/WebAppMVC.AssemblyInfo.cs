120e5f726c58ced50519cd31a16baa4044106b67
KlausJung011 <nicolas.nsavs@gmail.com>
2025-09-11 00:53:52 -0400

    Cambio links en header y mejora index de producto

--- WebAppMVC/obj/Debug/net8.0/WebAppMVC.AssemblyInfo.cs
+++ WebAppMVC/obj/Debug/net8.0/WebAppMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e6017c6424043d7154a2970731eae88db936411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c9f8ae5bef900391661dd9e37a3cd8b7a03344")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
